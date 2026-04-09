681c49ec54dc0dafc7c9428d703caf83b5ab0600
daniel5041 <dani_13@unicah.edu>
2026-04-08 18:33:13 -0600

    Create login.php
    creacion de login.php

---
+++ src/Controllers/usuario/login.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Controllers\usuario;
+
+use Controllers\PublicController;
+
+class login extends PublicController {
+
+    public function run(): void {
+
+        // 👇 ESTO SIEMPRE VA (para que NO salga blanco)
+        $this->viewName = "usuario/login";
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $usuarioModel = new \UsuarioModel();
+
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $usuario = $usuarioModel->obtenerUsuarioPorEmail($email);
+
+            if ($usuario && password_verify($password, $usuario->password)) {
+
+                $_SESSION['usuario_id'] = $usuario->id;
+                $_SESSION['usuario_nombre'] = $usuario->nombre;
+                $_SESSION['usuario_rol'] = $usuario->role;
+
+                echo "Login correcto";
+
+            } else {
+                echo "Correo o contraseña incorrectos";
+            }
+        }
+    }
+}
